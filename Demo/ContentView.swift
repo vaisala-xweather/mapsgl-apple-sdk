57b17ea0e2cd740cad452ea195677914f6509943
Slipp Douglas Thompson <slipp.thompson@aerisweather.com>
2024-02-07 18:22:42 -0600

    feat: Initial xcodeproj setup for MapsGL Demo App
    * Created App named “Demo” from Xcode 15.2 multiplatform App template.

---
+++ Demo/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Slipp Douglas Thompson on 2/7/24.
+//
+
+import SwiftUI
+
+
+
+struct ContentView : View
+{
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundStyle(.tint)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
+}
+
+
+
+#Preview {
+	ContentView()
+}
---
+++ Demo/DemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Slipp Douglas Thompson on 2/7/24.
+//
+
+import SwiftUI
+
+
+
+@main
+struct DemoApp : App
+{
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
